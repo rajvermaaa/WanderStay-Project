92cca69a45fbc8fd3d3c305f576e829c798091ef
rajvermaaa <rajverma02604@gmail.com>
2024-10-03 22:35:09 +0530

    added Payment features and updated the show.ejs page to look more glamourous

--- models/listing.js
+++ models/listing.js
@@ -16,6 +16,7 @@ const listingSchema = new Schema({
     price: Number,
     location: String,
     country: String,  
+    contact: Number,
     reviews: [{
         type: Schema.Types.ObjectId,
         ref: "Review",
--- schema.js
+++ schema.js
@@ -8,6 +8,7 @@ module.exports.listingSchema = Joi.object({
             country: Joi.string().required(),
             price: Joi.number().required().min(0),
             image: Joi.string().allow("",null),
+            contact: Joi.number().required().min(0),
         }).required()
 });
 
