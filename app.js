2bdc19f13ccf1979f35a8de6530872f1244482b0
Raj Verma <rajverma02604@gmail.com>
2025-11-16 00:04:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.use((req, res, next) =>  {
 
 
 
-// app.use("/listings", listingRouter);
+app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", listingRouter);
  
