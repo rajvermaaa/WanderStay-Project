8b29723b240a48014c976988c14e0ceaf931ecb3
rajvermaaa <intern.mongoosetech@gmail.com>
2025-11-15 18:31:29 +0530

    added search feature 2

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(express.static(path.join(__dirname, "/public" )));
 
 
 const store = MongoStore.create({
-    mongoUrl: MONGO_URL,
+    mongoUrl: dbUrl,
     crypto: {
         secret: process.env.SECRET,
     },
